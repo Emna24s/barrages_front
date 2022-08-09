c032a65298613a8d2ade3dc93467747617d22740
tarakj <tarek.jarboui@tnpost.tn>
2022-08-09 09:33:36 +0100

    init v0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {single} from './data';
+import {TodoService} from "./todo.service";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testngx';
+  single: any[] = [];
+  view: [number, number] = [700, 400];
+
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'X';
+  showYAxisLabel = true;
+  yAxisLabel = 'Y';
+
+  colorScheme = {
+    domain: ['red', 'blue']
+  };
+
+  constructor(private todoService: TodoService) {
+    Object.assign(this, {single})
+  }
+
+  onSelect(event: any[]) {
+    console.log(event);
+  }
+
+
 }
+
---
+++ src/app/data.ts
@@ -0,0 +1,14 @@
+export var single = [
+  {
+    "name": "A",
+    "value": 10
+  },
+  {
+    "name": "B",
+    "value": 50
+  },
+  {
+    "name": "C",
+    "value": 13
+  }
+];
